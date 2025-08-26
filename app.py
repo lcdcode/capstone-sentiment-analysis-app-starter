eb1abc68ed81e7a6d0abbffdad1fde8f36c75a23
lcdcode <lcd-code@mailbox.org>
2025-08-26 23:31:10 +0000

    Add Jupyter notebook for building a custom Keras model for sentiment analysis
    - Downloaded and unzipped the sentiment labelled sentences dataset from UCI
    - Loaded data from Yelp, Amazon, and IMDB into a single DataFrame
    - Tokenized the text data and padded sequences for model input
    - Split the dataset into training and testing sets
    - Created a Sequential Keras model with an embedding layer, LSTM layer, and output layer
    - Trained the model with early stopping based on validation accuracy
    - Saved the trained model and tokenizer to files

--- app.py
+++ app.py
@@ -1,9 +1,47 @@
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import sequence
+
 from flask import Flask, render_template, request
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # TODO: Write the code that calls the sentiment analysis functions here.
-    # hint: use request.method == "POST"
-    return render_template('form.html')
+    if request.method == "POST":
+        text = request.form.get("user_text")
+    else:
+        text = ""
+    sentiment = SentimentIntensityAnalyzer()
+    sentiment_score = sentiment.polarity_scores(text)
+    sentiment = sentiment.polarity_scores(text) # VADER results
+    # create a new key in the dictionary to store the custom model sentiment analysis results
+    load_tokenizer()
+    load_keras_model()
+    sentiment["custom model positive"] = sentiment_analysis(text)
+    return render_template('form.html', sentiment=sentiment)
+
+model = None
+tokenizer = None
+
+def load_keras_model():
+    global model
+    model = load_model('models/uci_sentimentanalysis.h5')
+
+def load_tokenizer():
+    global tokenizer
+    with open('models/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+# @app.before_first_request
+def before_first_request():
+    load_keras_model()
+    load_tokenizer()
+
+def sentiment_analysis(input):
+    user_sequences = tokenizer.texts_to_sequences([input])
+    user_sequences_matrix = sequence.pad_sequences(user_sequences, maxlen=1225)
+    prediction = model.predict(user_sequences_matrix)
+    return round(float(prediction[0][0]),2)
+
 if __name__ == "__main__":
     app.run()
